d37ef5c60bdf4300e92b1a316b79c940916f9ca0
Nguyen Tan Alex Le <2494377@edu.vaniercollege.qc.ca>
2025-03-03 13:22:10 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    /*
+
+    1. F
+    2. F
+    3. F
+    4. T
+    5. T
+
+    1.
+    2.
+    3.
+    4.
+    5.
+
+    1.
+    2.
+    3.
+    4.
+    5.
+
+    1.
+    2.
+    3.
+    4.
+    5.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
