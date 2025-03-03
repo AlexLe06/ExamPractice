83a24622f61e7ca001df061f5c3a8fb242cf5e02
2494377 <tanalexle06@gmail.com>
2025-03-03 13:53:44 -0500

    Method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-public class Main {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
     /*
 
@@ -14,11 +15,11 @@ public class Main {
     4.B
     5.C
 
-    1.
-    2.
-    3.
-    4.
-    5.
+    1. extends
+    2. default
+    3. Comparator
+    4. ArrayList<Long> longInt = new ArrayList<>();
+    5. FileWriter (not scanner)
 
     1.
     2.
@@ -26,7 +27,25 @@ public class Main {
     4.
     5.
      */
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ArrayList<Long> longInt = new ArrayList<Long>();
+    }
+
+    /**
+     * modifies the array by mutliplying each element by the factor
+     * @param nums the input array
+     * @param factor the input factor to multiply with
+     */
+    public static void multiply(double[] nums, double factor) {
+        if (nums == (null)) {
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) { //cant use enhanced for loop because need to modify element
+            nums[i] *= factor;
+        }
     }
 }
